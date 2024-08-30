7887794d7507751ec9514a46c7ea46dc11ec518a
Petr Sima <ptea.sima@gmail.com>
2024-08-30 16:14:57 +0200

    Initial Commit

---
+++ Literae/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Literae
+//
+//  Created by Petr Šíma on 8/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Literae/LiteraeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiteraeApp.swift
+//  Literae
+//
+//  Created by Petr Šíma on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LiteraeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
